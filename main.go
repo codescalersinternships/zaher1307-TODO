efe42a5650fc1ec2b52374a34e9bbe8520eee22b
zaher1307 <ahmedzaher1307@gmail.com>
2022-07-27 22:04:26 +0200

    basic api created

---
+++ main.go
@@ -0,0 +1,168 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+const (
+	DB_FILE = "DBFILE.db"
+	LISTEN_PORT    = ":8080"
+)
+
+type Server struct {
+	db *gorm.DB
+}
+
+type Todo struct {
+    ID       int64  `json:"id"`
+    TodoItem string `json:"todoItem"`
+    Complete bool   `json:"complete"`
+}
+
+func (db *Server) GetTodoItemsList(w http.ResponseWriter, r *http.Request) {
+
+    var todoList []Todo
+	
+	if result := db.db.Find(&todoList) ;result.Error != nil {
+		http.Error(w, result.Error.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	data, err := json.Marshal(todoList)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(data)
+
+}
+
+func (db *Server) GetTodoItem(w http.ResponseWriter, r *http.Request) {
+
+	var todo Todo
+	vars := mux.Vars(r)
+	id := vars["id"]
+	
+	if result := db.db.First(&todo, id); result.Error != nil {
+		http.Error(w, result.Error.Error(), http.StatusNotFound)
+		return
+	}
+
+	data, err := json.Marshal(todo)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(data)
+
+}
+
+func (db *Server) AddTodoItem(w http.ResponseWriter, r *http.Request) {
+
+	var todo Todo
+	if err := json.NewDecoder(r.Body).Decode(&todo); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return 
+	}
+
+	if result := db.db.First(&todo, todo.ID); result.Error == nil {
+		http.Error(w, result.Error.Error(), http.StatusConflict)
+		return
+	}
+
+	if result := db.db.Create(&todo); result.Error != nil {
+		http.Error(w, result.Error.Error(), http.StatusConflict)
+		return 
+	}
+
+	w.WriteHeader(http.StatusCreated)
+
+}
+
+func (db *Server) UpdateTodoItem(w http.ResponseWriter, r *http.Request) {
+
+	var newTodo Todo
+	vars := mux.Vars(r)
+	id, _ := strconv.ParseInt(vars["id"], 10, 64)
+
+	if err := json.NewDecoder(r.Body).Decode(&newTodo); err != nil || newTodo.ID != id {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return 
+	}
+
+	var oldTodo Todo
+	if result := db.db.First(&oldTodo, newTodo.ID); result.Error != nil {
+		http.Error(w, result.Error.Error(), http.StatusConflict)
+		return
+	}
+
+	if result := db.db.Save(&newTodo); result.Error != nil {
+		http.Error(w, result.Error.Error(), http.StatusConflict)
+		return 
+	}
+
+	w.WriteHeader(http.StatusAccepted)
+
+}
+
+func (db *Server) DeleteTodoItem(w http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	var todo Todo
+	if result := db.db.First(&todo, id); result.Error != nil {
+		http.Error(w, result.Error.Error(), http.StatusConflict)
+		return
+	}
+
+	if result := db.db.Delete(&todo); result.Error != nil {
+		http.Error(w, result.Error.Error(), http.StatusConflict)
+		return 
+	}
+
+	w.WriteHeader(http.StatusAccepted)
+
+}
+
+
+func main () {
+
+	s := Server{}
+	var err error
+	
+	if s.db, err = gorm.Open(sqlite.Open(DB_FILE), &gorm.Config{}); err != nil {
+		fmt.Println("Error")
+		return 
+	}
+	s.db.AutoMigrate(&Todo{})
+
+	router := mux.NewRouter()
+	router.HandleFunc("/todolist", s.GetTodoItemsList).Methods("GET")
+	router.HandleFunc("/todolist", s.AddTodoItem).Methods("POST")
+	router.HandleFunc("/todolist/{id}", s.GetTodoItem).Methods("GET")
+	router.HandleFunc("/todolist/{id}", s.UpdateTodoItem).Methods("PATCH")
+	router.HandleFunc("/todolist/{id}", s.DeleteTodoItem).Methods("DELETE")
+
+	http.ListenAndServe(LISTEN_PORT, router)
+
+}
+
+
+
+
+
+
